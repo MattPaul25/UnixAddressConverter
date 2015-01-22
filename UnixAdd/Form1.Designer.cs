307b01df9c13a52e0037687e31477f541581eac2
Matt Farguson <matt.farguson@gmail.com>
2015-01-22 15:51:53 -0800

    first commit

---
+++ UnixAdd/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace UnixAdd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(35, 56);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(621, 20);
+            this.txtAddress.TabIndex = 0;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(35, 31);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(0, 13);
+            this.lblInstructions.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 146);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.txtAddress);
+            this.Name = "Form1";
+            this.Text = "Unix Address Converter";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ UnixAdd/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnixAdd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblInstructions.Text = "Paste Your windows address in here and it will convert it to unix (for GitBash)";
+        }
+        private string textClean(string addressTxt)
+        {
+            string newAddress = "";
+
+            if (addressTxt != "")
+            {
+                newAddress = addressTxt.Replace(@"\", "/");
+                newAddress = newAddress.Replace(" ", @"\ ");
+                txtAddress.Text = newAddress;
+                Clipboard.SetText(newAddress);
+            }
+            return newAddress;
+        }
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            string myWinAdd = Clipboard.GetText();
+            txtAddress.Text = textClean(myWinAdd);
+        }
+    }
+}
